51b186301858130e927750c074250c6dfa503a40
Niklas-Liljendahl <niklas.liljendahl@gmail.com>
2024-01-30 23:30:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const testFunction = () => {
+    console.log('CONSOLE.LOG!')
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="pinpoint-widget" data-apikey="hej123" data-onpress="test" data-submit={testFunction} />
       </header>
     </div>
   );
